cdfd4fa1f6ccd1c5c7eb101310f9dfff494a0854
Disease <83523587+CPScript@users.noreply.github.com>
2024-10-14 18:00:52 -0400

    Create conf_firewall.sh

---
+++ scripts/conf_firewall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Configuring firewall..."
+sudo iptables -A INPUT -p tcp --dport 8888 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 1080 -j ACCEPT
+sudo iptables -A INPUT -p udp --dport 1194 -j ACCEPT
+sudo iptables -A INPUT -p udp --dport 51820 -j ACCEPT
+echo "Firewall configured."
