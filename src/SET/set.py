88b3a13a2dd2e36c9b6d9e5639145da094b3acbb
Disease <83523587+CPScript@users.noreply.github.com>
2024-10-14 19:45:22 -0400

    Update set.py

--- src/SET/set.py
+++ src/SET/set.py
@@ -1,4 +1,50 @@
 import curses
+import subprocess
+
+def settings_menu(stdscr):
+    curses.curs_set(0)
+    stdscr.clear()
+    stdscr.refresh()
+
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
+    curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
+
+    menu = ["Logging", "Firewall Rules", "Return to Main Menu"]
+    current_row = 0
+
+    while True:
+        stdscr.clear()
+        stdscr.addstr(0, 0, "Settings")
+        for idx, row in enumerate(menu):
+            if idx == current_row:
+                stdscr.attron(curses.color_pair(1))
+                stdscr.addstr(idx + 1, 0, row)
+                stdscr.attroff(curses.color_pair(1))
+            else:
+                stdscr.addstr(idx + 1, 0, row)
+
+        key = stdscr.getch()
+
+        if key == curses.KEY_UP and current_row > 0:
+            current_row -= 1
+        elif key == curses.KEY_DOWN and current_row < len(menu) - 1:
+            current_row += 1
+        elif key == curses.KEY_ENTER or key in [10, 13]:
+            if current_row == len(menu) - 1:
+                break 
+            elif current_row == 0:
+                bashCommand = "bash scripts/configure_logging.sh"
+                process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+                output, error = process.communicate()
+                stdscr.addstr(len(menu) + 1, 0, "Logging configured.")
+            elif current_row == 1:
+                bashCommand = "bash scripts/configure_firewall.sh"
+                process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+                output, error = process.communicate()
+                stdscr.addstr(len(menu) + 1, 0, "Firewall rules configured.")
+
+        stdscr.refresh()import curses
 
 def settings_menu(stdscr):
     stdscr.clear()
@@ -11,12 +57,12 @@ def settings_menu(stdscr):
 
     setting = input("Enter setting to configure (logging/firewall): ")
     if setting == "logging":
-        bashCommand = "bash scripts/configure_logging.sh"
+        bashCommand = "bash scripts/conf_logging.sh"
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
         stdscr.addstr(4, 0, "Logging configured.")
     elif setting == "firewall":
-        bashCommand = "bash scripts/configure_firewall.sh"
+        bashCommand = "bash scripts/conf_firewall.sh"
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
         stdscr.addstr(4, 0, "Firewall rules configured.")
