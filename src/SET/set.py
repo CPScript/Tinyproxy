dbdd394539ae5288e6277be40cef019bdca670ad
Disease <83523587+CPScript@users.noreply.github.com>
2024-10-14 19:45:42 -0400

    Update set.py

--- src/SET/set.py
+++ src/SET/set.py
@@ -1,7 +1,7 @@
 import curses
 import subprocess
 
-def settings_menu(stdscr):
+def proxy_menu(stdscr):
     curses.curs_set(0)
     stdscr.clear()
     stdscr.refresh()
@@ -10,12 +10,12 @@ def settings_menu(stdscr):
     curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
     curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
-    menu = ["Logging", "Firewall Rules", "Return to Main Menu"]
+    menu = ["HTTP Proxy", "SOCKS Proxy", "Return to Main Menu"]
     current_row = 0
 
     while True:
         stdscr.clear()
-        stdscr.addstr(0, 0, "Settings")
+        stdscr.addstr(0, 0, "Proxy Setup - Choose Type")
         for idx, row in enumerate(menu):
             if idx == current_row:
                 stdscr.attron(curses.color_pair(1))
@@ -32,41 +32,16 @@ def settings_menu(stdscr):
             current_row += 1
         elif key == curses.KEY_ENTER or key in [10, 13]:
             if current_row == len(menu) - 1:
-                break 
+                break  
             elif current_row == 0:
-                bashCommand = "bash scripts/configure_logging.sh"
+                bashCommand = "bash scripts/setup_proxy.sh http"
                 process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
                 output, error = process.communicate()
-                stdscr.addstr(len(menu) + 1, 0, "Logging configured.")
+                stdscr.addstr(len(menu) + 1, 0, "HTTP Proxy setup complete.")
             elif current_row == 1:
-                bashCommand = "bash scripts/configure_firewall.sh"
+                bashCommand = "bash scripts/setup_proxy.sh socks"
                 process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
                 output, error = process.communicate()
-                stdscr.addstr(len(menu) + 1, 0, "Firewall rules configured.")
+                stdscr.addstr(len(menu) + 1, 0, "SOCKS Proxy setup complete.")
 
-        stdscr.refresh()import curses
-
-def settings_menu(stdscr):
-    stdscr.clear()
-    stdscr.addstr(0, 0, "Settings")
-    stdscr.addstr(1, 0, "1. Logging")
-    stdscr.addstr(2, 0, "2. Firewall Rules")
-    stdscr.addstr(3, 0, "Press any key to return to main menu.")
-    stdscr.refresh()
-    stdscr.getch()
-
-    setting = input("Enter setting to configure (logging/firewall): ")
-    if setting == "logging":
-        bashCommand = "bash scripts/conf_logging.sh"
-        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
-        stdscr.addstr(4, 0, "Logging configured.")
-    elif setting == "firewall":
-        bashCommand = "bash scripts/conf_firewall.sh"
-        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
-        stdscr.addstr(4, 0, "Firewall rules configured.")
-    else:
-        stdscr.addstr(4, 0, "Invalid setting. Please try again.")
         stdscr.refresh()
-        stdscr.getch()
