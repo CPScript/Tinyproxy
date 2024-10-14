f2a585c0d6a21ba94abb8194979f41b0d384c23b
Disease <83523587+CPScript@users.noreply.github.com>
2024-10-14 19:44:07 -0400

    Update menu.py

--- src/menu.py
+++ src/menu.py
@@ -1,12 +1,17 @@
 import curses
+import os
 from PROXY.proxy import proxy_menu
 from VPN.vpn import vpn_menu
-from SET.set import settings_menu
+from SETT.set import settings_menu
 
 def main_menu(stdscr):
-    curses.curs_set(0)
+    curses.curs_set(0)  
     stdscr.clear()
     stdscr.refresh()
+
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)  
+    curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)  
 
     menu = ['Set up Proxy', 'Set up VPN', 'Settings', 'Exit']
     current_row = 0
@@ -15,11 +20,13 @@ def main_menu(stdscr):
         stdscr.clear()
         for idx, row in enumerate(menu):
             if idx == current_row:
-                stdscr.attron(curses.color_pair(1))
+                stdscr.attron(curses.color_pair(1)) 
                 stdscr.addstr(idx, 0, row)
-                stdscr.attroff(curses.color_pair(1))
+                stdscr.attroff(curses.color_pair(1))  
             else:
+                stdscr.attron(curses.color_pair(2))  
                 stdscr.addstr(idx, 0, row)
+                stdscr.attroff(curses.color_pair(2))  
 
         key = stdscr.getch()
 
@@ -29,7 +36,7 @@ def main_menu(stdscr):
             current_row += 1
         elif key == curses.KEY_ENTER or key in [10, 13]:
             if current_row == len(menu) - 1:
-                break
+                break  
             elif current_row == 0:
                 proxy_menu(stdscr)
             elif current_row == 1:
