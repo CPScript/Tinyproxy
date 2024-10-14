24fa95e091d692c1114be417e04f5996d63d6db6
Disease <83523587+CPScript@users.noreply.github.com>
2024-10-14 18:48:15 -0400

    Update conf_firewall.sh

--- scripts/conf_firewall.sh
+++ scripts/conf_firewall.sh
@@ -3,6 +3,6 @@
 echo "Configuring firewall..."
 sudo iptables -A INPUT -p tcp --dport 8888 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 1080 -j ACCEPT
-sudo iptables -A INPUT -p udp --dport 1194 -j ACCEPT
+sudo iptables -A INPUT -p udp -- dport 1194 -j ACCEPT
 sudo iptables -A INPUT -p udp --dport 51820 -j ACCEPT
 echo "Firewall configured."
