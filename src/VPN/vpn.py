a08066a63930d968ab2df20b4c0db57dacbea243
Disease <83523587+CPScript@users.noreply.github.com>
2024-10-14 19:44:33 -0400

    Update vpn.py

--- src/VPN/vpn.py
+++ src/VPN/vpn.py
@@ -1,4 +1,50 @@
 import curses
+import subprocess
+
+def vpn_menu(stdscr):
+    curses.curs_set(0)
+    stdscr.clear()
+    stdscr.refresh()
+
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
+    curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
+
+    menu = ["OpenVPN", "WireGuard", "Return to Main Menu"]
+    current_row = 0
+
+    while True:
+        stdscr.clear()
+        stdscr.addstr(0, 0, "VPN Setup - Choose Type")
+        for idx, row in enumerate(menu):
+            if idx == current_row:
+                stdscr.attron(curses.color_pair(1))
+                stdscr.addstr(idx + 1, 0, row)
+                stdscr.attroff(curses.color_pair(1))
+            else:
+                stdscr.addstr(idx + 1, 0, row)
+
+        key = stdscr.getch()
+
+        if key == curses.KEY_UP and current_row > 0:
+            current_row -= 1
+        elif key == curses.KEY_DOWN and current_row < len(menu) - 1:
+            current_row += 1
+        elif key == curses.KEY_ENTER or key in [10, 13]:
+            if current_row == len(menu) - 1:
+                break 
+            elif current_row == 0:
+                bashCommand = "bash scripts/setup_vpn.sh openvpn"
+                process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+                output, error = process.communicate()
+                stdscr.addstr(len(menu) + 1, 0, "OpenVPN setup complete.")
+            elif current_row == 1:
+                bashCommand = "bash scripts/setup_vpn.sh wireguard"
+                process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+                output, error = process.communicate()
+                stdscr.addstr(len(menu) + 1, 0, "WireGuard setup complete.")
+
+        stdscr.refresh()import curses
 
 def vpn_menu(stdscr):
     stdscr.clear()
