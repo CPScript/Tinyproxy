51f35110fb2d54b015955d68480ef6de66b1b170
Disease <83523587+CPScript@users.noreply.github.com>
2024-10-14 18:29:02 -0400

    Update setup_proxy.sh

--- scripts/setup_proxy.sh
+++ scripts/setup_proxy.sh
@@ -2,15 +2,17 @@
 
 setup_http_proxy() {
     echo "Configuring Tinyproxy..."
-    sudo sed -i 's/Allow 127.0.0.1/Allow ALL/' /etc/tinyproxy/tinyproxy.conf
+    sudo sed -i 's/^#Allow 127.0.0.1/Allow 0.0.0.0/' /etc/tinyproxy/tinyproxy.conf
     sudo systemctl restart tinyproxy
     echo "HTTP Proxy setup complete. Connect using your server's IP and port 8888."
 }
 
 setup_socks_proxy() {
     echo "Configuring 3proxy..."
-    echo "socks -p1080" > ~/3proxy.cfg
-    3proxy ~/3proxy.cfg
+    cat <<EOF > ~/3proxy.cfg
+socks -p1080
+EOF
+    3proxy ~/3proxy.cfg &
     echo "SOCKS Proxy setup complete. Connect using your server's IP and port 1080."
 }
 
