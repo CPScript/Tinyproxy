f3642376ea52305e7c03db906a38ff5d2a00ebc1
Disease <83523587+CPScript@users.noreply.github.com>
2024-10-14 23:50:17 -0400

    Delete scripts directory

--- scripts/conf_logging.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Configuring logging..."
-sudo mkdir -p /var/log/proxy-vpn-setup
-sudo touch /var/log/proxy-vpn-setup/setup.log
-sudo chmod 600 /var/log/proxy-vpn-setup/setup.log
-
-echo "Logging configured."
